3a9216e68b682c8e8d23378f4d5ecab02486cf9e
brunna <brunnad68@gmail.com>
2024-04-13 18:56:30 -0300

    first commit

---
+++ contador.py
@@ -0,0 +1,58 @@
+"""
+Aqui é fornecido uma lista de email atraves do arquivo conta.xlsx
+e faz o comparativo com base.xlsx
+
+RESULTADO:
+1 - Ele verifica e tras no console a quantidade de 'ATIVOS', 'DESATIVADOS', e também diz se não encontrou, assim como as porcentagem e total
+2 - Diz o status dos emails, e por fim apenas a coluna do status.
+"""
+
+
+import pandas as pd
+
+# Caminhos dos arquivos
+base_data_path = 'base.xlsx'
+emails_to_check_path = 'conta.xlsx'
+
+
+
+# Definição da função para verificar o status dos e-mails
+def check_status(email, df_base):
+    match = df_base[df_base['Email Address [Required]'] == email]['Last Sign In [READ ONLY]']
+    if not match.empty:
+        return 'DESATIVADO' if match.iloc[0] == 'Never logged in' else 'ATIVO'
+    else:
+        return 'EMAIL NÃO ENCONTRADO'
+
+# Carregando os dados das planilhas
+df_base = pd.read_excel(base_data_path)
+df_emails_to_check = pd.read_excel(emails_to_check_path)
+
+# Selecionando a primeira coluna de 'conta.xlsx', que corresponde à coluna 'A'
+email_column = df_emails_to_check.columns[0]  # Isso pega o nome da primeira coluna
+
+# Aplicando a função para verificar o status dos e-mails em 'conta.xlsx'
+df_emails_to_check['Status'] = df_emails_to_check[email_column].apply(lambda email: check_status(email, df_base))
+
+# Calculando as estatísticas baseado nos status dos e-mails verificados
+status_counts = df_emails_to_check['Status'].value_counts()
+total_emails = df_emails_to_check.shape[0]
+status_percentage = (status_counts / total_emails) * 100
+
+# Adicionando a contagem total
+status_counts['TOTAL'] = total_emails
+
+# Exibindo os resultados no console
+print("------------------------------")
+print("Quantitativo e Porcentagem de Status baseado em 'conta.xlsx':")
+print(status_counts)
+print(status_percentage)
+
+# Exibindo o status de cada e-mail em 'conta.xlsx' com o nome da coluna dinâmico
+print("------------------------------")
+print("\nStatus dos e-mails da planilha 'conta':")
+print(df_emails_to_check[[email_column, 'Status']])
+
+print("---------------------------")
+print("---------------------------")
+print(df_emails_to_check['Status'].to_string(index=False))
